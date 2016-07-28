2217286f7ff846b6c3517de6859b40d27bef2275
Alex Chan <alex@twilio.com>
2016-07-28 01:02:17 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+	# Basic package information.
+	name = 'jira',
+	version = '0.0.0',
+	packages = ['jira'],
+	include_package_data = True,
+	install_requires = ['httplib2', 'simplejson'],
+	license='LICENSE.txt',
+	url = 'https://github.com/alexcchan/jira/tree/master',
+	keywords = 'jira api',
+	description = 'JIRA API v2 Wrapper for Python',
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Internet'
+	],
+)
+
+
